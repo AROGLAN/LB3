b2eabac0b552a1c6523308f6b3f6b69ee56909e1
AROGLAN <130068722+AROGLAN@users.noreply.github.com>
2023-04-19 12:58:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+	<title>LB3</title>
+</head>
+<bode>
+<b> Session of the selected client </b>
+<p>
+                <label for="student">Choose a client</label>
+                <select id="student" name="name">
+                    <option value="student1">Іванов Д.</option>
+                    <option value="student2">Корольков І.</option>
+                    <option value="student3">Карманьський Б.</option>
+                    <option value="student4">Воронін Р.</option>
+                </select>
+                <button name="button">Get info</button>
+            </p>
+ Network sessions for a specified period of time
+ 
+     <form>
+ <p>
+  <label>Start time </label>
+  <input type="text">
+  <br>
+  <br>
+  <label>End time</label>
+  <input type="text">
+  </p>
+  <button name="button">Get info</button>
+</form>       
+<br>
+  List of debtors
+  <button name="button">Get info</button>
+            
+            
+            
+
+
+
+
+</bode>
